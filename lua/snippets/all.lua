666473af6552df4fd5b0acdcc85b0cc1b0f4909a
rashad <rashadalsharpini@gmail.com>
2025-04-23 13:47:07 +0200

    fixing todo

--- lua/snippets/all.lua
+++ lua/snippets/all.lua
@@ -1,9 +1,7 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local t = ls.text_node
 local f = ls.function_node
 
--- Function to get the correct comment prefix based on filetype
 local function comment_prefix()
     local ft = vim.bo.filetype
     local comment_map = {
@@ -18,10 +16,11 @@ local function comment_prefix()
         go = "// TODO: ",
         cs = "// TODO: ",
     }
-    return comment_map[ft] or "-- TODO: " -- Default comment style
+    return { comment_map[ft] or "-- TODO: " }
 end
 
--- Define the TODO snippet for all file types
-ls.add_snippets("all", {
-    s("todo", { f(comment_prefix, {}), t("") })
-})
+return {
+    s("todo", {
+        f(comment_prefix, {}),
+    }),
+}
