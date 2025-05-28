194ea97e46bab0fe9ac8937cecda0e06c66c1be7
rashad <rashadalsharpini@gmail.com>
2025-05-28 22:49:50 +0300

    upd

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -6,3 +6,10 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd({"BufReadPost", "BufNewFile"}, {
+  callback = function()
+    vim.opt_local.foldmethod = "indent"
+    vim.opt_local.foldlevel = 99   -- unfold all folds
+  end,
+})
