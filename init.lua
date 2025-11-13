b991f2304bdf610f6866a5387f923341b4f71894
rashad <rashadalsharpini@gmail.com>
2025-11-13 13:24:09 +0200

    upd

--- init.lua
+++ init.lua
@@ -2,5 +2,3 @@ require("config")
 require("config.lazy")
 require("config.remap")
 require("plugins")
-vim.opt.clipboard = "unnamedplus"
-
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -6,10 +6,11 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
 
-vim.api.nvim_create_autocmd({"BufReadPost", "BufNewFile"}, {
-  callback = function()
-    vim.opt_local.foldmethod = "indent"
-    vim.opt_local.foldlevel = 99   -- unfold all folds
-  end,
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+	callback = function()
+		vim.opt_local.foldmethod = "indent"
+		vim.opt_local.foldlevel = 99 -- unfold all folds
+	end,
 })
