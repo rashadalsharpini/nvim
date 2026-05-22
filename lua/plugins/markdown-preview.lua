539b9b6c58cc2d5183aeef1deab9d29f447f0778
rashad <rashadalsharpini@gmail.com>
2026-05-22 15:27:31 +0300

    markdown-preview

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,9 @@
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = "cd app && npm install",
+    config = function()
+        -- We will put configuration options here next
+    end,
+}
